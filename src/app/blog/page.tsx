a1980f453391bfea9a59da7e26a8d3d564c221b4
Donald Jennings <D.D.Jennings@outlook.com>
2026-03-16 17:42:21 +0000

    Fix blog/project routing performance and 500s without MDX compile

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,5 @@
 import BlogLandingPage from '@/components/pages/BlogLandingPage';
 
-export const dynamic = 'force-dynamic';
-
 export default async function BlogPage() {
   return <BlogLandingPage />;
 }
